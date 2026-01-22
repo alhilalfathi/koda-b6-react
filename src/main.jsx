2ec5059137433e8c9c731ac9cbe2488cbf120dbd
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-22 09:38:01 +0700

    init: initialize file react

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from "react-dom/client";
+import "./assets/css/style.css"
+import { useState } from "react";
+
+
+const el = document.getElementById("root")
+const root = createRoot(el)
+
+
+
+
+root.render()
