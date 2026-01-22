029ebcc0f0c5e3ccb13803483e02c5a65d7df5da
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-22 20:45:39 +0700

    feat: add MainDiv, InputDiv, ButtonDiv,LoginByDiv into Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,15 +1,22 @@
+import { InputDiv } from "../component/InputDiv"
+import { MainDiv } from "../component/MainDiv"
+import { IoPersonOutline } from "react-icons/io5";
+import { HiOutlineMail } from "react-icons/hi";
+import { GoKey } from "react-icons/go";
+import { ButtonDiv } from "../component/buttonDiv";
+import { LoginByDiv } from "../component/LoginByDiv";
 
-export const MainDiv = ()=>{
+
+export const RegisterPage = ()=>{
 
     return (
-        <div className="flex">
-            <div className="w-1-3">
-                <img className="h-screen" src="src/assets/img/Rectangle289.png" alt="" />
-            </div>
-            <div className="w-2/3 mx-40 my-20 flex flex-col gap-6">
-                <img className="w-74" src="src/assets/img/Frame12.png" alt="" />
-                <h1 className="">Register</h1>
-            </div>
-        </div>
+        <MainDiv img={"src/assets/img/Rectangle289.png"} imgname={"main-image"} title={"Register"} desctitle={"Fill out the form correctly"}>
+            <InputDiv type={"text"} id={"name"} name={"name"} icon={<IoPersonOutline />} placeholder={"Enter Your Full Name"}>Full Name</InputDiv>
+            <InputDiv type={"email"} id={"email"} name={"email"} icon={<HiOutlineMail />} placeholder={"Enter Your Email"}>Email</InputDiv>
+            <InputDiv type={"password"} id={"password"} name={"password"} icon={<GoKey />} placeholder={"Enter Your Password"}>Password</InputDiv>
+            <InputDiv type={"password"} id={"confirmPassword"} name={"confirmPassword"} icon={<GoKey />} placeholder={"Enter Your Password Again"}>Confirm Password</InputDiv>
+            <ButtonDiv>Register</ButtonDiv>
+            <LoginByDiv />
+        </MainDiv>
     )
 }
