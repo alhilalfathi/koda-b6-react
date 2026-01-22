1bcbf8eace6469498bccfaa45f00d0a6c69934ce
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-22 10:18:04 +0700

    feat: divide register component from main.jsx

---
+++ src/pages/Register.jsx
@@ -0,0 +1,15 @@
+
+export const MainDiv = ()=>{
+
+    return (
+        <div className="flex">
+            <div className="w-1-3">
+                <img className="h-screen" src="src/assets/img/Rectangle289.png" alt="" />
+            </div>
+            <div className="w-2/3 mx-40 my-20 flex flex-col gap-6">
+                <img className="w-74" src="src/assets/img/Frame12.png" alt="" />
+                <h1 className="">Register</h1>
+            </div>
+        </div>
+    )
+}
