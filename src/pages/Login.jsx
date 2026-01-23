fc49bcef03e45ffdc95d480f324c037e828e8c9f
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-23 23:45:10 +0700

    feat: add eye icon for password input

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,7 @@ import { GoKey } from "react-icons/go";
 import { ButtonDiv } from "../component/buttonDiv";
 import { LoginByDiv } from "../component/LoginByDiv";
 import { Link } from "react-router-dom";
+import { FiEye } from "react-icons/fi";
 
 
 
@@ -12,7 +13,7 @@ export const LoginPage = ()=>{
     return(
         <MainDiv img={"src/assets/img/Rectangle289-1.png"} imgname={"main-image"} title={"Login"} desctitle={"Fill out the form correctly"}>
             <InputDiv type={"email"} id={"email"} name={"email"} icon={<HiOutlineMail />} placeholder={"Enter Your Email"}>Email</InputDiv>
-            <InputDiv type={"password"} id={"password"} name={"password"} icon={<GoKey />} placeholder={"Enter Your Password"}>Password</InputDiv>
+            <InputDiv type={"password"} id={"password"} name={"password"} icon={<GoKey />} placeholder={"Enter Your Password"} eye={<FiEye />}>Password</InputDiv>
             <Link className="text-orange-600 flex justify-end" to="/forgot-password">Forgot Password?</Link>
             <ButtonDiv>Login</ButtonDiv>
             <LoginByDiv />
