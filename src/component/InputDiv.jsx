460fe87ceb4240a6b137450a5ff87aad216e66ac
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-23 23:44:30 +0700

    feat: add new props for eye icon

--- src/component/InputDiv.jsx
+++ src/component/InputDiv.jsx
@@ -5,6 +5,7 @@ export const InputDiv = (input)=>{
             <div className="flex items-center bg-stone-50 border-2 border-stone-200 h-10 px-3 rounded-lg">
                 {input.icon}
                 <input className="mx-4 w-full" type={input.type} id={input.id} name={input.name} placeholder={input.placeholder}/>
+                {input.eye}
             </div>
         </div>
     )
