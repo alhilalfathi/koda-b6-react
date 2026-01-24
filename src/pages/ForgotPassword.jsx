33b9c8cd76795394ecf655862bedb9f9450bb2c2
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-24 15:17:47 +0700

    feat: image src using import variable

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -2,11 +2,11 @@ import { InputDiv } from "../component/InputDiv";
 import { MainDiv } from "../component/MainDiv";
 import { HiOutlineMail } from "react-icons/hi";
 import { ButtonDiv } from "../component/buttonDiv";
-
+import forgotPassImage from "../assets/img/Rectangle289-2.png"
 
 export const ForgotPass = ()=>{
     return(
-        <MainDiv img={"src/assets/img/Rectangle289-2.png"} imgname={"main-image"} title={"Fill out the form correctly"} desctitle={"We will send new password to your email"}>
+        <MainDiv img={forgotPassImage} imgname={"main-image"} title={"Fill out the form correctly"} desctitle={"We will send new password to your email"}>
             <InputDiv type={"email"} id={"email"} name={"email"} icon={<HiOutlineMail />} placeholder={"Enter Your Email"}>Email</InputDiv>
             <ButtonDiv>Submit</ButtonDiv>        
         </MainDiv>
