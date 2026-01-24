cab6392ea7bf548f9d1849d3cb21f7efee453eaa
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-24 16:54:19 +0700

    feat: add header home component

---
+++ src/component/HomeHeader.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom"
+import mainImg from "../assets/img/Rectangle287.png"
+
+
+export const HomeHeader = () => {
+  return (
+    <div className="flex">
+        <main className="w-1/2 h-auto bg-zinc-950">
+            <div className="text-white px-30 py-40">
+                <h1 className="font-bold text-5xl mb-10">Start Your Day with Coffee and Good Meals</h1>
+                <p>We provide high quality beans, good taste, and healthy meals made by love just fot you. Start your day with us for a bigger smile!</p>
+                <div className="w-28 h-10 bg-orange-600 mt-10 flex items-center justify-center text-black rounded">
+                    <Link>Get Started</Link>
+                </div>
+                <div className="flex gap-8 mt-10">
+                    <div className="flex flex-col border-4 border-zinc-950 border-r-white pr-4">
+                        <div className="text-orange-600 font-bold text-5xl">90+</div>
+                        <div>Staff</div>
+                    </div>
+                    <div className="flex flex-col border-4 border-zinc-950 border-r-white pr-4 ">
+                        <div className="text-orange-600 font-bold text-5xl">30+</div>
+                        <div>Stores</div>
+                    </div>
+                    <div className="flex flex-col">
+                        <div className="text-orange-600 font-bold text-5xl">800+</div>
+                        <div>Customer</div>
+                    </div>
+                </div>
+            </div>
+            
+        </main>
+
+        <aside className="w-1/2">
+            <img src={mainImg} alt="main-img" />
+        </aside>
+    </div>
+  )
+}
