7b0f41ce2f6af45b6a90fe688c1050fb22470a0f
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-25 11:54:38 +0700

    fix: adjust responsive scale from md to lg

--- src/component/HomeHeader.jsx
+++ src/component/HomeHeader.jsx
@@ -4,10 +4,10 @@ import mainImg from "../assets/img/Rectangle287.png"
 
 export const HomeHeader = () => {
   return (
-    <div className="flex flex-col md:flex-row">
-        <main className="w-full md:w-1/2 h-auto bg-zinc-950">
-            <div className="text-white px-6 py-20 md:px-30 md:py-40">
-                <h1 className="font-bold text-3xl md:text-5xl mb-6 md:mb-10">Start Your Day with Coffee and Good Meals</h1>
+    <div className="flex flex-col lg:flex-row">
+        <main className="w-full lg:w-1/2 h-auto bg-zinc-950">
+            <div className="text-white px-6 py-20 lg:px-30 lg:py-40">
+                <h1 className="font-bold text-3xl lg:text-5xl mb-6 lg:mb-10">Start Your Day with Coffee and Good Meals</h1>
                 <p>We provide high quality beans, good taste, and healthy meals made by love just fot you. Start your day with us for a bigger smile!</p>
                 <div className="w-28 h-10 bg-orange-600 mt-10 flex items-center justify-center text-black rounded">
                     <Link>Get Started</Link>
@@ -30,7 +30,7 @@ export const HomeHeader = () => {
             
         </main>
 
-        <aside className="hidden md:block md:w-1/2">
+        <aside className="hidden lg:block lg:w-1/2">
             <img src={mainImg} alt="main-img" />
         </aside>
     </div>
