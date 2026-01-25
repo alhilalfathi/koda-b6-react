c00757fff3f96b64bfff2db90a7411f0c5d7a4e8
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-25 14:31:55 +0700

    fix: split footer into component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,15 @@ import { HomeNav } from "../component/HomeNav";
 import sideImage from "../assets/img/Rectangle291.png"
 import checklistIcon from "../assets/img/Vector.png"
 import { BsCart2 } from "react-icons/bs";
+import { Footer } from "../component/Footer";
 
 export const HomePage = ()=>{
     return(
         <div>
             <HomeNav />
+
             <HomeHeader />
+            
             <section className="flex flex-col md:flex-row">
                         {/* header */}
                 <div className="w-full md:w-1/2 px-6 md:px-20 py-10">
@@ -131,35 +134,7 @@ export const HomePage = ()=>{
                 </div>
             </section>
 
-            <footer>
-                <div className="flex px-20 py-10 gap-40">
-                    <div className="w-78 flex flex-col gap-4 justify-center" >
-                        <img src="src/assets/img/Frame12.png" alt="logo coffee shop"/>
-                        <p>Coffee Shop is a store that sells some good meals, and especially coffee. We provide high quality beans</p>
-                        <p className="text-zinc-400 py-10">@2020CoffeeStore</p>
-                    </div>
-                    <ul className="flex flex-col gap-4">
-                        <li className="font-bold">Product</li>
-                        <li>Our Ptroduct</li>
-                        <li>Pricing</li>
-                        <li>Locations</li>
-                        <li>Countries</li>
-                        <li>Blog</li>
-                    </ul>
-                    <ul className="flex flex-col gap-4">
-                        <li className="font-bold">Engage</li>
-                        <li>Partner</li>
-                        <li>FAQ</li>
-                        <li>About Us</li>
-                        <li>Privacy Policy</li>
-                        <li>Terms of Service</li>
-                    </ul>
-                    <div className="flex gap-4 flex-col">
-                        <p className="pl-4 font-bold">Social Media</p>
-                        <img src="src/assets/img/Frame42.png" alt="sosmed"/>
-                    </div>
-                </div>
-            </footer>
+            <Footer />
         </div>
     )
 }
