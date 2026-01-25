a0dce5c3564d31e289e588ac423cf89b6ce778af
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-25 11:40:57 +0700

    feat: add responsive feature for small screnn

--- src/component/HomeHeader.jsx
+++ src/component/HomeHeader.jsx
@@ -4,10 +4,10 @@ import mainImg from "../assets/img/Rectangle287.png"
 
 export const HomeHeader = () => {
   return (
-    <div className="flex">
-        <main className="w-1/2 h-auto bg-zinc-950">
-            <div className="text-white px-30 py-40">
-                <h1 className="font-bold text-5xl mb-10">Start Your Day with Coffee and Good Meals</h1>
+    <div className="flex flex-col md:flex-row">
+        <main className="w-full md:w-1/2 h-auto bg-zinc-950">
+            <div className="text-white px-6 py-20 md:px-30 md:py-40">
+                <h1 className="font-bold text-3xl md:text-5xl mb-6 md:mb-10">Start Your Day with Coffee and Good Meals</h1>
                 <p>We provide high quality beans, good taste, and healthy meals made by love just fot you. Start your day with us for a bigger smile!</p>
                 <div className="w-28 h-10 bg-orange-600 mt-10 flex items-center justify-center text-black rounded">
                     <Link>Get Started</Link>
@@ -30,7 +30,7 @@ export const HomeHeader = () => {
             
         </main>
 
-        <aside className="w-1/2">
+        <aside className="hidden md:block md:w-1/2">
             <img src={mainImg} alt="main-img" />
         </aside>
     </div>
