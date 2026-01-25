0f7b91308a323b0d8e23b7cd97d9c61d26e5b16e
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-25 11:40:40 +0700

    feat: add responsive feature for small screnn

--- src/component/HomeNav.jsx
+++ src/component/HomeNav.jsx
@@ -3,21 +3,27 @@ import { Link } from "react-router-dom"
 export const HomeNav = ()=>{
     return(
     
-        <div className="w-full h-16 flex justify-between bg-black/10 absolute">
-            <div className="flex items-center gap-6 px-8 ml-10">
+        <div className="w-full h-16 flex justify-between bg-black/10 absolute px-4 md:px-10">
+            <div className="flex items-center gap-6">
                 <div >
-                    <img className="w-auto" src="src/assets/img/Frame13.png" alt="logo-kopi"/>
+                    <img className="w-auto h-8" src="src/assets/img/Frame13.png" alt="logo-kopi"/>
                 </div>
-                <ul className="text-white flex gap-6">
+                <ul className="text-white hidden md:flex gap-6">
                     <li><Link to="/">Home</Link></li>
                     <li> <Link to="/product">Product</Link></li>
                 </ul>
             </div>
-            <div className="flex gap-6 text-white items-center gap-6 px-8 mr-10">
+            <div className="hidden md:flex text-white items-center gap-6">
                     <img className="w-6 h-6" src="src/assets/img/Search.png"  alt="logo-search"/>
                     <img className="w-6 h-6" src="src/assets/img/ShoppingCart.png"  alt="logo-keranjang"/>
-                <Link className="w-22 h-10 border flex items-center justify-center" to="/login">SignIn</Link>
-                <Link className="w-22 h-10 bg-orange-600 flex items-center justify-center" to="/register">Sign Up</Link>
+                <Link className="w-24 h-10 border flex items-center justify-center" to="/login">SignIn</Link>
+                <Link className="w-24 h-10 bg-orange-600 flex items-center justify-center" to="/register">Sign Up</Link>
+            </div>
+
+            <div className="md:hidden flex items-center">
+                <button className="text-white text-2xl">
+                    &#9776;
+                </button>                
             </div>
         </div>
 
