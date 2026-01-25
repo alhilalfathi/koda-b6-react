49a91445598e8d84d5b22434ef0896d1d6b8542e
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-25 23:08:10 +0700

    feat: add pagination component

---
+++ src/component/Pagination.jsx
@@ -0,0 +1,13 @@
+
+
+export const Pagination = () => {
+  return (
+    <div className="flex gap-3">
+        <div className="bg-[#FF8906] rounded-full w-6 flex justify-center">1</div>
+        <div className="bg-[#E8E8E8] rounded-full w-6 flex justify-center text-[#A0A3BD]">2</div>
+        <div className="bg-[#E8E8E8] rounded-full w-6 flex justify-center text-[#A0A3BD]">3</div>
+        <div className="bg-[#E8E8E8] rounded-full w-6 flex justify-center text-[#A0A3BD]">4</div>
+        <div className="bg-[#FF8906] rounded-full w-6 flex justify-center text-white">&#8594;</div>
+    </div>
+  )
+}
