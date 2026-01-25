25be498587d147b9c9fcd626268e00a4fcba4665
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-25 11:22:58 +0700

    feat: add responsive feature for small screnn

--- src/component/MainDiv.jsx
+++ src/component/MainDiv.jsx
@@ -1,11 +1,11 @@
 export const MainDiv = (main)=>{
 
     return (
-        <div className="flex">
-            <div className="w-1/3">
-                <img className="h-full w-full" src={main.img} alt={main.imgname} />
+        <div className="flex flex-col md:flex-row min-h-screen">
+            <div className="hidden md:block md:w-1/3">
+                <img className="h-64 md:h-full w-full" src={main.img} alt={main.imgname} />
             </div>
-            <div className="w-2/3 mx-40 flex flex-col gap-4">
+            <div className="w-full px-6 md:w-2/3 md:mx-40 py-8 flex flex-col gap-4">
                 <img className="w-74 my-10" src="src/assets/img/Frame12.png" alt="logo" />
                 <div className="flex flex-col gap-4">
                     <h1 className="text-orange-900 font-bold text-2xl">{main.title}</h1>
