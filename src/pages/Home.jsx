c4a49a3a312181b471d806e3e761d91fd65e082a
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-25 14:26:56 +0700

    feat: add content to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,165 @@
 import { HomeHeader } from "../component/HomeHeader";
 import { HomeNav } from "../component/HomeNav";
-
+import sideImage from "../assets/img/Rectangle291.png"
+import checklistIcon from "../assets/img/Vector.png"
+import { BsCart2 } from "react-icons/bs";
 
 export const HomePage = ()=>{
     return(
         <div>
             <HomeNav />
             <HomeHeader />
-            
+            <section className="flex flex-col md:flex-row">
+                        {/* header */}
+                <div className="w-full md:w-1/2 px-6 md:px-20 py-10">
+                    <h1 className="text-3xl md:text-6xl font-medium">We Provide <span className="text-yellow-700">Good Cofee</span> and <span className="text-yellow-700">Healthy Meals</span></h1>
+                    <p className="my-10">You can explore the menu that we provide with fun and have their own taste and make your day better.</p>
+                    <div className="flex flex-col gap-6">
+                        <div className="flex gap-4"><img className="w-6 h-6" src={checklistIcon} alt="checklist icon"/><p>High quality beans</p></div>
+                        <div className="flex gap-4"><img className="w-6 h-6" src={checklistIcon} alt="checklist icon"/><p>Healthy meals, you can request the ingerdient</p></div>
+                        <div className="flex gap-4"><img className="w-6 h-6" src={checklistIcon} alt="checklist icon"/><p>Chat with our staff to get better experience for ordering</p></div>
+                        <div className="flex gap-4"><img className="w-6 h-6" src={checklistIcon} alt="checklist icon"/><p>Free member card with a minimum purchase of IDR 200.000.</p></div>
+                    </div>
+                </div>
+                        {/* aside */}
+                <div className="w-full md:w-1/2"><img src={sideImage} alt="staff image" /></div>
+            </section>
+
+            <section>
+                        {/* header */}
+                <div className="flex flex-col justify-center items-center mt-10">
+                    <h1 className="text-3xl md:text-6xl font-medium">Here is People's <span className="text-yellow-700">Favorite</span></h1>
+                    <p className="mt-10 mb-20">Let's choose and have a bit taste of people's favorite. It might be yours too!</p>
+                </div>
+                        {/* product list */}
+                <div className="flex justify-center items-center gap-2">
+                        {/* product */}
+                    <div className="flex flex-col relative">
+                        <div className="w-64" >
+                            <img src="src/assets/img/image27.png" alt="product image"/>
+                        </div>
+                        <div className="w-58 bg-white absolute top-52 left-3 p-2" >
+                            <h3 className="text-2xl mb-2">Hazelnut Latte</h3>
+                            <p className="text-sm text-stone-700 mb-2">You can explore the menu that we provide with fun and have their own taste and make your day better.</p>
+                            <h3 className="text-xl text-orange-600 mb-2">IDR 20.000</h3>
+                            <div className="flex gap-3">
+                                <a className="w-44 h-10 bg-orange-600 rounded flex items-center justify-center" href="">Buy</a>
+                                <div className="w-12 flex justify-center items-center border-1 border-orange-600 rounded">
+                                    <BsCart2 />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col relative">
+                        <div className="w-64" >
+                            <img src="src/assets/img/image27.png" alt="product image"/>
+                        </div>
+                        <div className="w-58 bg-white absolute top-52 left-3 p-2" >
+                            <h3 className="text-2xl mb-2">Hazelnut Latte</h3>
+                            <p className="text-sm text-stone-700 mb-2">You can explore the menu that we provide with fun and have their own taste and make your day better.</p>
+                            <h3 className="text-xl text-orange-600 mb-2">IDR 20.000</h3>
+                            <div className="flex gap-3">
+                                <a className="w-44 h-10 bg-orange-600 rounded flex items-center justify-center" href="">Buy</a>
+                                <div className="w-12 flex justify-center items-center border-1 border-orange-600 rounded">
+                                    <BsCart2 />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col relative">
+                        <div className="w-64" >
+                            <img src="src/assets/img/image27.png" alt="product image"/>
+                        </div>
+                        <div className="w-58 bg-white absolute top-52 left-3 p-2" >
+                            <h3 className="text-2xl mb-2">Hazelnut Latte</h3>
+                            <p className="text-sm text-stone-700 mb-2">You can explore the menu that we provide with fun and have their own taste and make your day better.</p>
+                            <h3 className="text-xl text-orange-600 mb-2">IDR 20.000</h3>
+                            <div className="flex gap-3">
+                                <a className="w-44 h-10 bg-orange-600 rounded flex items-center justify-center" href="">Buy</a>
+                                <div className="w-12 flex justify-center items-center border-1 border-orange-600 rounded">
+                                    <BsCart2 />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col relative">
+                        <div className="w-64" >
+                            <img src="src/assets/img/image27.png" alt="product image"/>
+                        </div>
+                        <div className="w-58 bg-white absolute top-52 left-3 p-2" >
+                            <h3 className="text-2xl mb-2">Hazelnut Latte</h3>
+                            <p className="text-sm text-stone-700 mb-2">You can explore the menu that we provide with fun and have their own taste and make your day better.</p>
+                            <h3 className="text-xl text-orange-600 mb-2">IDR 20.000</h3>
+                            <div className="flex gap-3">
+                                <a className="w-44 h-10 bg-orange-600 rounded flex items-center justify-center" href="">Buy</a>
+                                <div className="w-12 flex justify-center items-center border-1 border-orange-600 rounded">
+                                    <BsCart2 />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                       {/* header */}
+                <div className="flex flex-col justify-center items-center mt-70 bg-zinc-50 pt-20">
+                    <h1 className="text-3xl md:text-6xl font-medium text-center"> <span className="text-yellow-700">Visit Our Store</span> in the Spot on the Map Below</h1>
+                    <p className="mt-10 w-128 text-center">You can explore the menu that we provide with fun and have their own taste and make your day better.</p>
+                </div>
+                <div className="flex justify-center items-center"><img className="w-300 " src="src/assets/img/HugeGlobal.png" alt="maps image" /></div>
+
+            </section>
+
+            <section>
+                <div className="flex bg-black p-20 gap-4">
+                    <div>
+                        <img src="src/assets/img/Rectangle295.png" alt="295"/>
+                    </div>
+                    <div className="text-white">
+                        <p className="pb-4">TESTIMONIAL</p>
+                        <h2 className="text-3xl border border-black border-l-orange-600 pl-4 py-4">Viezh Robert</h2>
+                        <p className="text-orange-600 pt-4">Manager Coffe Shop</p>
+                        <p className="w-128 py-4">“Wow... I am very happy to spend my whole day here. the Wi-fi is good, and the coffee and meals tho. I like it here!! Very recommended!</p>
+                        <div><img src="src/assets/img/Frame41.png" alt="41"/></div>
+                        <div className="flex gap-4 py-6">
+                            <div className="w-12 h-12 bg-white rounded-full text-black flex justify-center items-center text-3xl font-bold">&#8592;</div>
+                            <div className="w-12 h-12 bg-orange-600 rounded-full text-black flex justify-center items-center text-3xl font-bold">&#8594;</div>
+                        </div>
+                        <div><img src="src/assets/img/Group1300.png" alt="1300"/></div>
+                    </div>
+                </div>
+            </section>
+
+            <footer>
+                <div className="flex px-20 py-10 gap-40">
+                    <div className="w-78 flex flex-col gap-4 justify-center" >
+                        <img src="src/assets/img/Frame12.png" alt="logo coffee shop"/>
+                        <p>Coffee Shop is a store that sells some good meals, and especially coffee. We provide high quality beans</p>
+                        <p className="text-zinc-400 py-10">@2020CoffeeStore</p>
+                    </div>
+                    <ul className="flex flex-col gap-4">
+                        <li className="font-bold">Product</li>
+                        <li>Our Ptroduct</li>
+                        <li>Pricing</li>
+                        <li>Locations</li>
+                        <li>Countries</li>
+                        <li>Blog</li>
+                    </ul>
+                    <ul className="flex flex-col gap-4">
+                        <li className="font-bold">Engage</li>
+                        <li>Partner</li>
+                        <li>FAQ</li>
+                        <li>About Us</li>
+                        <li>Privacy Policy</li>
+                        <li>Terms of Service</li>
+                    </ul>
+                    <div className="flex gap-4 flex-col">
+                        <p className="pl-4 font-bold">Social Media</p>
+                        <img src="src/assets/img/Frame42.png" alt="sosmed"/>
+                    </div>
+                </div>
+            </footer>
         </div>
     )
 }
