862eecdb446c79ba0d4989885f1953e0971a16ac
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-29 17:23:02 +0700

    feat: add home button underline

--- src/component/HomeNav.jsx
+++ src/component/HomeNav.jsx
@@ -10,7 +10,7 @@ export const HomeNav = ()=>{
                     <img className="w-auto h-8" src="src/assets/img/Frame13.png" alt="logo-kopi"/>
                 </div>
                 <ul className="text-white hidden md:flex gap-6">
-                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/" className="underline underline-offset-8 decoration-[#FF8906]">Home</Link></li>
                     <li> <Link to="/product">Product</Link></li>
                 </ul>
             </div>
@@ -18,8 +18,8 @@ export const HomeNav = ()=>{
             <div className="hidden md:flex text-white items-center gap-6">
                     <img className="w-6 h-6" src="src/assets/img/Search.png"  alt="logo-search"/>
                     <img className="w-6 h-6" src="src/assets/img/ShoppingCart.png"  alt="logo-keranjang"/>
-                <Link className="w-24 h-10 border flex items-center justify-center" to="/login">SignIn</Link>
-                <Link className="w-24 h-10 bg-[#FF8906] flex items-center justify-center" to="/register">Sign Up</Link>
+                <Link className="w-24 h-10 border flex items-center justify-center rounded" to="/login">SignIn</Link>
+                <Link className="w-24 h-10 bg-[#FF8906] flex items-center justify-center rounded" to="/register">Sign Up</Link>
             </div>
             {/* hamburger menu  */}
             <div className="md:hidden flex items-center">
