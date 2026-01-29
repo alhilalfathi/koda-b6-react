102ee2f39b5437d5e61bbb4c82c625accb650212
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-29 17:24:19 +0700

    feat: add nav product button underline

--- src/component/NavDiv.jsx
+++ src/component/NavDiv.jsx
@@ -10,15 +10,15 @@ export const NavDiv = () => {
                 </div>
                 <ul className="text-white hidden md:flex gap-6">
                     <li><Link to="/">Home</Link></li>
-                    <li> <Link to="/product">Product</Link></li>
+                    <li> <Link to="/product" className="underline underline-offset-8 decoration-[#FF8906]">Product</Link></li>
                 </ul>
             </div>
             {/* right navbar  */}
             <div className="hidden md:flex text-white items-center gap-6">
                     <img className="w-6 h-6" src="src/assets/img/Search.png"  alt="logo-search"/>
                     <img className="w-6 h-6" src="src/assets/img/ShoppingCart.png"  alt="logo-keranjang"/>
-                <Link className="w-24 h-10 border flex items-center justify-center" to="/login">SignIn</Link>
-                <Link className="w-24 h-10 bg-[#FF8906] flex items-center justify-center" to="/register">Sign Up</Link>
+                <Link className="w-24 h-10 border flex items-center justify-center rounded" to="/login">SignIn</Link>
+                <Link className="w-24 h-10 bg-[#FF8906] flex items-center justify-center rounded" to="/register">Sign Up</Link>
             </div>
             {/* hamburger menu  */}
             <div className="md:hidden flex items-center">
