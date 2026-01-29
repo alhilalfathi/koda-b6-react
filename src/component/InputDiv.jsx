cbc1815f977314a694dbe84d8252670914409472
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-29 09:36:08 +0700

    feat: add padding on input

--- src/component/InputDiv.jsx
+++ src/component/InputDiv.jsx
@@ -4,7 +4,7 @@ export const InputDiv = (input)=>{
             <label htmlFor={input.id} className="font-bold my-2 text-lg" >{input.children}</label>
             <div className="flex items-center bg-stone-50 border-2 border-stone-200 h-10 px-3 rounded-lg">
                 {input.icon}
-                <input className="mx-4 w-full" type={input.type} id={input.id} name={input.name} placeholder={input.placeholder}/>
+                <input className="mx-4 w-full px-3 py-2" type={input.type} id={input.id} name={input.name} placeholder={input.placeholder}/>
                 {input.eye}
             </div>
         </div>
