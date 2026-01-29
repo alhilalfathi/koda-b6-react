26d58a4bd149c98e98ea9e48cf36d01f356f92eb
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-29 16:55:01 +0700

    feat: add buy and cart button

--- src/pages/DetailProduct.jsx
+++ src/pages/DetailProduct.jsx
@@ -2,6 +2,8 @@ import { Footer } from "../component/Footer"
 import { NavDiv } from "../component/NavDiv"
 import { AiFillLike } from "react-icons/ai";
 import { Product } from "../component/ProductDiv";
+import cartIcon from "../assets/img/ShoppingCart-yellow.png"
+import { Pagination } from "../component/Pagination";
 
 
 export const DetailProduct = () => {
@@ -10,16 +12,16 @@ export const DetailProduct = () => {
         <NavDiv />
         <div className="flex md:flex-row flex-col mx-20 my-10 gap-4">
             {/* side image  */}
-            <div className="flex flex-col w-[50%] gap-4 ">
-                <img className="md:w-150 w-full md:h-150" src="src/assets/img/image31.png" alt="product image" />
-                <div className="md:flex gap-3 hidden">
-                    <img className="md:w-48 w-15 h-15 md:h-48" src="src/assets/img/image31.png" alt="product image" />
-                    <img className="md:w-48 w-15 h-15 md:h-48" src="src/assets/img/image31.png" alt="product image" />
-                    <img className="md:w-48 w-15 h-15 md:h-48" src="src/assets/img/image31.png" alt="product image" />
+            <div className="flex flex-col w-1/2 gap-4 ">
+                <img className=" w-full md:h-150" src="src/assets/img/image31.png" alt="product image" />
+                <div className="md:flex gap-3 hidden md:items-center md:justify-center">
+                    <img className="md:w-[32%] w-15 h-15 md:h-full" src="src/assets/img/image31.png" alt="product image" />
+                    <img className="md:w-[32%] w-15 h-15 md:h-full" src="src/assets/img/image31.png" alt="product image" />
+                    <img className="md:w-[32%] w-15 h-15 md:h-full" src="src/assets/img/image31.png" alt="product image" />
                 </div>
             </div>
             {/* content  */}
-            <div>
+            <div className="w-1/2">
                 <span className="bg-red-600 text-white rounded-xl w-26  flex items-center justify-center">FLASH SALE!</span>
                 <h1 className="md:text-4xl text-xl py-3">Hazelnut Latte</h1>
                 <div className="flex gap-2 items-center">
@@ -40,15 +42,22 @@ export const DetailProduct = () => {
                 </div>
                 <h3 className="font-bold text-xl">Choose Size</h3>
                 <div className="flex gap-5 py-3">
-                    <button className="w-40 h-8 border border-[#FF8906]">Regular</button>
-                    <button className="w-40 h-8 border border-[#E8E8E8]">Medium</button>
-                    <button className="w-40 h-8 border border-[#E8E8E8]">Large</button>
+                    <button className="w-1/3 h-8 border border-[#FF8906]">Regular</button>
+                    <button className="w-1/3 h-8 border border-[#E8E8E8]">Medium</button>
+                    <button className="w-1/3 h-8 border border-[#E8E8E8]">Large</button>
                 </div>
                 <h3 className="font-bold text-xl">Hot/Ice?</h3>
                 <div className="flex gap-5 py-3">
-                    <button className="w-64 h-8 border border-[#FF8906]">Ice</button>
-                    <button className="w-64 h-8 border border-[#E8E8E8]">Hot</button>
+                    <button className="w-1/2 h-8 border border-[#FF8906]">Ice</button>
+                    <button className="w-1/2 h-8 border border-[#E8E8E8]">Hot</button>
                     
+                </div>
+                <div className="flex gap-5 my-5">
+                    <button className="bg-[#FF8906] w-1/2 p-3 rounded cursor-pointer">Buy</button>
+                    <span className="flex gap-3 w-1/2 border border-[#FF8906] p-3 justify-center rounded cursor-pointer">
+                        <img src={cartIcon} alt="cart"></img>
+                        <button>Add to cart</button>
+                    </span>
                 </div>
             </div>
         </div>
@@ -60,6 +69,9 @@ export const DetailProduct = () => {
                 <Product />
                 <Product />
             </div>
+        </div>
+        <div className="my-15 w-full flex justify-center">
+            <Pagination />
         </div>
         <Footer />
     </div>
