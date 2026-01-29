55ba8e8bd2ae67fc92a9c1e2ce05f21c4cdf20b7
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-29 09:37:19 +0700

    feat: add routing to profil page

--- src/Router.jsx
+++ src/Router.jsx
@@ -9,6 +9,7 @@ import { DetailProduct } from "./pages/DetailProduct";
 import { HistoryOrder } from "./pages/HistoryOrder";
 import { CheckoutProduct } from "./pages/CheckoutProduct";
 import { DetailOrder } from "./pages/DetailOrder";
+import { Profile } from "./pages/Profile";
 
 
 const router = createBrowserRouter([
@@ -51,6 +52,10 @@ const router = createBrowserRouter([
     {
         path: "/detail-order",
         element: <DetailOrder />
+    },
+    {
+        path: "/profile",
+        element: <Profile />
     }
 ])
 
