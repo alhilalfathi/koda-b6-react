2d76e4774f779fc06b1a5570353d81b90f5b1672
alhilalfathi <alhilal.fathi@gmail.com>
2026-02-01 08:43:35 +0700

    fix: change image source from direct to import

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,10 +1,11 @@
-
+import logo from "../assets/img/Frame12.png"
+import sosmed from "../assets/img/Frame42.png"
 
 export const Footer = () => {
   return (
     <div className="flex flex-col md:flex-row px-20 py-10 gap-40 bg-[#F8F8F8]">
         <div className="w-78 flex flex-col gap-4 justify-center" >
-            <img src="src/assets/img/Frame12.png" alt="logo coffee shop"/>
+            <img src={logo} alt="logo coffee shop"/>
             <p>Coffee Shop is a store that sells some good meals, and especially coffee. We provide high quality beans</p>
             <p className="text-zinc-400 py-10">@2020CoffeeStore</p>
         </div>
@@ -26,7 +27,7 @@ export const Footer = () => {
         </ul>
         <div className="flex gap-4 flex-col">
             <p className="pl-4 font-bold">Social Media</p>
-            <img className="w-30 md:w-unset" src="src/assets/img/Frame42.png" alt="sosmed"/>
+            <img className="w-30 md:w-unset" src={sosmed} alt="sosmed"/>
         </div>
     </div>
   )
