dfc16a4858acd2ec947ccd78496fcf274e099e72
alhilalfathi <alhilal.fathi@gmail.com>
2026-02-02 01:00:19 +0700

    feat: add insert user modal

--- src/pages/AdminUser.jsx
+++ src/pages/AdminUser.jsx
@@ -11,16 +11,26 @@ import { FiEye } from "react-icons/fi"
 import { GoKey, GoLocation } from "react-icons/go"
 import { HiOutlineMail } from "react-icons/hi"
 import { CiPhone } from "react-icons/ci"
+import { useRef } from "react"
+import { useForm } from "react-hook-form"
 
 
 export const AdminUser = () => {
+    const {register} = useForm()
+    const insertRef = useRef()
+    const showInsert = () => {
+        insertRef.current.classList.remove("hidden")
+    }
+    const hideInsert = () => {
+        insertRef.current.classList.add("hidden")
+    }
   return (
     <div className="py-10 relative">
         <div className="mr-15">
             <div className="flex justify-between items-center mb-6">
                 <div>
                     <h1 className="text-2xl font-semibold">User List</h1>
-                    <button className="mt-3 bg-orange-500 text-white px-4 py-2 rounded-lg flex items-center gap-2">
+                    <button onClick={showInsert} className="mt-3 bg-orange-500 text-white px-4 py-2 rounded-lg flex items-center gap-2 cursor-pointer">
                         <img src={addIcon} alt="add icon" className="md:w-4 w-2 md:h-4 h-2"/>
                         Add User
                     </button>
@@ -113,11 +123,11 @@ export const AdminUser = () => {
         </div>
 
         {/* insert user sidebar  default hidden  */}
-        <div className="absolute top-0 right-0 w-271 bg-black/40 min-h-screen hidden">
+        <div ref={insertRef} className="absolute top-0 right-0 w-271 bg-black/40 min-h-screen hidden">
             <div className="absolute top-0 right-0 w-100 min-h-screen border p-5 bg-white">
                 <span className="flex justify-between items-center">
                     <h1 className="text-2xl font-bold">Insert User</h1>
-                    <IoCloseCircleSharp />
+                    <IoCloseCircleSharp onClick={hideInsert} className="cursor-pointer" />
                 </span>
                 <label className="block text-sm mb-2">Image User</label>
                 <div className="w-12 h-12 border rounded-lg flex items-center justify-center mb-3">
@@ -127,11 +137,11 @@ export const AdminUser = () => {
                     Upload
                 </button>
 
-                <InputDiv type={"text"} id={"name"} name={"name"} icon={<IoPersonOutline />} placeholder={"Enter Your Full Name"}>Full Name</InputDiv>
-                <InputDiv type={"email"} id={"email"} name={"email"} icon={<HiOutlineMail />} placeholder={"Enter Your Email"}>Email</InputDiv>
-                <InputDiv type={"text"} id={"phone"} name={"phone"} icon={<CiPhone />} placeholder={"Enter Your Number"}>Phone</InputDiv>
-                <InputDiv type={"password"} id={"password"} name={"password"} icon={<GoKey />} placeholder={"Enter Your Password"} eye={<FiEye />}>Password</InputDiv>
-                <InputDiv type={"text"} id={"address"} name={"address"} icon={<GoLocation />} placeholder={"Enter Your Address"}>Address</InputDiv>
+                <InputDiv register={register} type={"text"} id={"name"} name={"name"} icon={<IoPersonOutline />} placeholder={"Enter Your Full Name"}>Full Name</InputDiv>
+                <InputDiv register={register} type={"email"} id={"email"} name={"email"} icon={<HiOutlineMail />} placeholder={"Enter Your Email"}>Email</InputDiv>
+                <InputDiv register={register} type={"text"} id={"phone"} name={"phone"} icon={<CiPhone />} placeholder={"Enter Your Number"}>Phone</InputDiv>
+                <InputDiv register={register} type={"password"} id={"password"} name={"password"} icon={<GoKey />} placeholder={"Enter Your Password"} eye={<FiEye />}>Password</InputDiv>
+                <InputDiv register={register} type={"text"} id={"address"} name={"address"} icon={<GoLocation />} placeholder={"Enter Your Address"}>Address</InputDiv>
 
                 <label className="block my-2 font-bold">Type of User</label>
                 <div className="flex gap-2 mb-4 ">
