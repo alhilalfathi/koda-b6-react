0d5c6ebb8b1831393501f5a452e58937392e9fc0
alhilalfathi <alhilal.fathi@gmail.com>
2026-02-02 08:27:58 +0700

    feat: add button chat function

--- src/component/Chat.jsx
+++ src/component/Chat.jsx
@@ -1,12 +1,17 @@
+import { useRef } from "react"
 import adminPicture from "/assets/img/Ellipse183.png"
 import userPicture from "/assets/img/Ellipse184.png"
 
 export const ChatWidget = () => {
+    const chatRef = useRef()
+    const chat = () => {
+        chatRef.current.classList.toggle("hidden")
+    }
   return (
-    <div className="fixed right-10 bottom-15 ">
+    <div className="fixed right-10 bottom-15 items-center ">
 
       {/* Chat Box */}
-        <article className="w-80 bg-white rounded-2xl mb-5">
+        <article ref={chatRef} className="w-80 bg-white rounded-2xl mb-5 hidden">
 
             {/* Header */}
             <div className="bg-[#FF8906] p-4 rounded-t-2xl flex items-center gap-3">
@@ -46,15 +51,15 @@ export const ChatWidget = () => {
             <div className="p-3 border-t flex items-center gap-2">
                 <input type="text" placeholder="Masukan Pesan Anda" className="w-3/4 px-3 py-2 border border-gray-300 rounded-lg text-sm outline-none" />
                 <button className="w-1/4 bg-[#FF8906] rounded-lg p-2 flex justify-center">
-                    <img src="src/assets/img/sent.png" alt="send" />
+                    <img src="/assets/img/sent.png" alt="send" />
                 </button>
             </div>
         </article>
 
       {/* Chat Button */}
-        <div className="w-14 h-14 bg-[#FF8906] rounded-full flex items-center justify-center cursor-pointer absolute right-0 bottom-[-2]">
-            <img src="src/assets/img/ChatCircleDots.png" alt="Chat" className="mt-1" />
-        </div>
+        <button onClick={chat} className="w-14 h-14 bg-[#FF8906] rounded-full flex items-center justify-center cursor-pointer absolute right-0 bottom-[-2]">
+            <img src="/assets/img/ChatCircleDots.png" alt="Chat" className="mt-1" />
+        </button>
 
     </div>
   )
