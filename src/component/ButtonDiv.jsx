c6c690b9109f02d6a7ff9118f736499ee8251c47
alhilalfathi <alhilal.fathi@gmail.com>
2026-02-04 09:27:47 +0700

    fix: destructuring props

--- src/component/ButtonDiv.jsx
+++ src/component/ButtonDiv.jsx
@@ -1,7 +1,7 @@
-export const ButtonDiv = (btn)=>{
+export const ButtonDiv = ({id, type, children})=>{
     return(
         <div className="bg-orange-500 rounded-lg h-12 flex justify-center items-center cursor-pointer">
-            <button className="cursor-pointer font-bold text-lg" id={btn.id} type={btn.type} >{btn.children}</button>
+            <button className="cursor-pointer font-bold text-lg" id={id} type={type} >{children}</button>
         </div>
     )
 }
