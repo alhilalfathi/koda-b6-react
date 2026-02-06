1f90b1c72351af6606aa1faf535f84bc7f5bf6eb
alhilalfathi <alhilal.fathi@gmail.com>
2026-02-06 15:01:09 +0700

    feat: add login navbar feature

--- src/component/NavDiv.jsx
+++ src/component/NavDiv.jsx
@@ -1,15 +1,18 @@
+import { useContext } from "react"
 import logo from "/assets/img/Frame13.png"
 import searchIcon from "/assets/img/Search.png"
 import cartIcon from "/assets/img/ShoppingCart.png"
 import { Link } from "react-router-dom"
+import { AuthContext } from "./context/AuthContext"
 
 export const NavDiv = () => {
-  return (
-    <div className="w-full h-16 flex justify-between bg-black px-4 md:px-20">
+    const { user, logout } = useContext(AuthContext)
+    return (
+        <div className="w-full h-16 flex justify-between bg-black px-4 md:px-20">
             {/* left navbar  */}
             <div className="flex items-center gap-6">
                 <div >
-                    <img className="w-auto h-8" src={logo} alt="logo-kopi"/>
+                    <img className="w-auto h-8" src={logo} alt="logo-kopi" />
                 </div>
                 <ul className="text-white hidden md:flex gap-6">
                     <li><Link to="/">Home</Link></li>
@@ -18,17 +21,31 @@ export const NavDiv = () => {
             </div>
             {/* right navbar  */}
             <div className="hidden md:flex text-white items-center gap-6">
-                    <img className="w-6 h-6 cursor-pointer" src={searchIcon}  alt="logo-search"/>
-                    <Link to="/checkout"><img className="w-6 h-6 cursor-pointer" src={cartIcon}  alt="logo-keranjang"/></Link>
-                <Link className="w-24 h-10 border flex items-center justify-center rounded" to="/login">SignIn</Link>
-                <Link className="w-24 h-10 bg-[#FF8906] flex items-center justify-center rounded" to="/register">Sign Up</Link>
+                <img className="w-6 h-6 cursor-pointer" src={searchIcon} alt="logo-search" />
+                {user && (<Link to="/checkout"><img className="w-6 h-6 cursor-pointer" src="/assets/img/ShoppingCart.png" alt="logo-keranjang" /></Link>)}
+                {!user ? (
+                    <>
+                        <Link className="w-24 h-10 border flex items-center justify-center rounded" to="/login">SignIn</Link>
+                        <Link className="w-24 h-10 bg-[#FF8906] flex items-center justify-center rounded" to="/register">Sign Up</Link>
+                    </>
+                ) : (
+                    <>
+                        <span className="text-sm">Hi, {user.name}</span>
+                        <button
+                            onClick={logout}
+                            className="w-24 h-10 border flex items-center justify-center rounded"
+                        >
+                            Logout
+                        </button>
+                    </>
+                )}
             </div>
             {/* hamburger menu  */}
             <div className="md:hidden flex items-center">
                 <button className="text-white text-2xl">
                     &#9776;
-                </button>                
+                </button>
             </div>
         </div>
-  )
+    )
 }
