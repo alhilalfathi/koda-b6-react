ea72b24203a9f1beb2cdda41d09be734026a35fc
alhilalfathi <alhilal.fathi@gmail.com>
2026-02-17 11:34:31 +0700

    feat: add dynamic pagination

--- src/component/Pagination.jsx
+++ src/component/Pagination.jsx
@@ -1,13 +1,29 @@
 
 
-export const Pagination = () => {
+export const Pagination = ({currentPage, totalPages, setCurrentPage}) => {
+  
+  const pages = [...Array(totalPages)].map((_, i) => i + 1)
+
   return (
     <div className="flex gap-3">
-        <div className="bg-[#FF8906] rounded-full w-6 flex justify-center">1</div>
-        <div className="bg-[#E8E8E8] rounded-full w-6 flex justify-center text-[#A0A3BD]">2</div>
-        <div className="bg-[#E8E8E8] rounded-full w-6 flex justify-center text-[#A0A3BD]">3</div>
-        <div className="bg-[#E8E8E8] rounded-full w-6 flex justify-center text-[#A0A3BD]">4</div>
-        <div className="bg-[#FF8906] rounded-full w-6 flex justify-center text-white">&#8594;</div>
+      {pages.map(page => (
+        <div
+        key={page}
+        onClick={() => setCurrentPage(page)}
+        className={`rounded-full w-6 flex justify-center cursor-pointer ${currentPage === page ? "bg-[#FF8906] text-white" : "bg-[#E8E8E8] text-[#A0A3BD]"}`}
+        >
+          {page}
+        </div>
+      ))}
+
+      {currentPage < totalPages && (
+        <div
+        onClick={() => setCurrentPage(currentPage + 1)} 
+        className="bg-[#FF8906] text-white rounded-full w-6 flex justify-center cursor-pointer"
+        >
+          &#8594;
+        </div>
+      )}
     </div>
   )
 }
