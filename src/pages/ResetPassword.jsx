0a72703ce93e4f64e0750cbb12fa4b6ed3c92bc6
alhilalfathi <alhilal.fathi@gmail.com>
2026-03-29 20:03:25 +0700

    feat: add reset-password page

---
+++ src/pages/ResetPassword.jsx
@@ -0,0 +1,83 @@
+import { InputDiv } from "../component/InputDiv";
+import { MainDiv } from "../component/MainDiv";
+import { GoKey, GoShieldCheck } from "react-icons/go";
+import { ButtonDiv } from "../component/ButtonDiv";
+import resetPassImage from "/assets/img/Rectangle289-2.png";
+import { useForm } from "react-hook-form";
+import http from "../lib/http.js";
+import { useNavigate, useLocation } from "react-router-dom";
+
+export const ResetPassword = () => {
+    const { 
+        handleSubmit, 
+        register, 
+        formState: { isSubmitting } 
+    } = useForm()
+    
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    const emailFromForgot = location.state?.email || ""
+
+    async function onResetPassword(value) {
+        try {
+            const response = await http("/auth/forgot-password", {
+                method: "PATCH",
+                body: {
+                    email: emailFromForgot,
+                    code: parseInt(value.code),
+                    password: value.password
+                }
+            })
+
+            if (response.success) {
+                alert("Password successfully updated! Please login with your new password")
+                navigate("/login")
+            } else {
+                alert(response.message || "Invalid code or email")
+            }
+        } catch (error) {
+            console.error("Reset Password Error:", error)
+            alert("Connection server error")
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onResetPassword)}>
+            <MainDiv 
+                img={resetPassImage} 
+                imgname={"main-image"} 
+                title={"Reset Password"} 
+                desctitle={`Updating password for: ${emailFromForgot}`}
+            >
+                {/* Input code OTP */}
+                <InputDiv 
+                    register={register} 
+                    type={"number"} 
+                    id={"code"} 
+                    name={"code"} 
+                    icon={<GoShieldCheck />} 
+                    placeholder={"Enter 5-digit code"}
+                >
+                    Reset Code
+                </InputDiv>
+
+                {/* Input New Password */}
+                <InputDiv 
+                    register={register} 
+                    type={"password"} 
+                    id={"password"} 
+                    name={"password"} 
+                    icon={<GoKey />} 
+                    placeholder={"Enter New Password"}
+                >
+                    New Password
+                </InputDiv>
+
+                <ButtonDiv type="submit" disabled={isSubmitting}>
+                    {isSubmitting ? "Updating..." : "Update Password"}
+                </ButtonDiv>
+            </MainDiv>
+        </form>
+    )
+}
