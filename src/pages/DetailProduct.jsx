e643cb3febb5483739d3e4c38c9e4e52f5efad47
alhilalfathi <alhilal.fathi@gmail.com>
2026-04-04 16:14:45 +0700

    chore: change cart source path

--- src/pages/DetailProduct.jsx
+++ src/pages/DetailProduct.jsx
@@ -91,7 +91,7 @@ export const DetailProduct = () => {
         }
 
         try {
-            const response = await http("/cart", {
+            const response = await http("/admin/cart", {
                 method: "POST",
                 body: payload
             })
