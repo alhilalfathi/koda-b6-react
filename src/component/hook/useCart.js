5a12746733da8eed7b18be79b5aa4f51bbc4435a
alhilalfathi <alhilal.fathi@gmail.com>
2026-04-05 10:32:23 +0700

    chore: add checking for user before clearing cart

--- src/component/hook/useCart.js
+++ src/component/hook/useCart.js
@@ -7,25 +7,21 @@ export const useCart = () => {
   const user = useSelector((state) => state.auth.user)
   const carts = useSelector((state) => state.cart.carts)
 
-  if (!user) {
-    alert("You need to login")
-    return {
-      cartItems: [],
-      currentUser: null
+  const cartItems = user ? (carts[user.email] || []) : []
+
+  const handleRemove = (index) => {
+    if (user) {
+      dispatch(removeFromCart({
+        email: user.email,
+        index
+      }))
     }
   }
 
-  const cartItems = carts[user.email] || []
-
-  const handleRemove = (index) => {
-    dispatch(removeFromCart({
-      email: user.email,
-      index
-    }))
-  }
-
   const handleClearCart = () => {
-    dispatch(clearCart(user.email))
+    if (user) {
+      dispatch(clearCart({ email: user.email }))
+    }
   }
 
   return {
