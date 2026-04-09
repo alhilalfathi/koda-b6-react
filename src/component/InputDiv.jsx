843b5568efc600679c5ae0139da7558ee221dc38
alhilalfathi <alhilal.fathi@gmail.com>
2026-04-09 20:09:12 +0700

    chore: add onChange and value

--- src/component/InputDiv.jsx
+++ src/component/InputDiv.jsx
@@ -1,19 +1,26 @@
 export const InputDiv = (input)=>{
     return(
         <div className="flex flex-col">
-            <label htmlFor={input.id} className="font-bold my-2 text-lg" >{input.children}</label>
+            <label htmlFor={input.id} className="font-bold my-2 text-lg">
+                {input.children}
+            </label>
+
             <div className="flex items-center bg-stone-50 border-2 border-stone-200 h-10 px-3 rounded-lg">
                 {input.icon}
+
                 <input 
-                className="mx-4 w-full px-3 py-2" 
-                type={input.type} 
-                id={input.id} 
-                placeholder={input.placeholder}
-                {...(input.register? input.register(input.name) : {})}
+                    className="mx-4 w-full px-3 py-2" 
+                    type={input.type} 
+                    id={input.id} 
+                    name={input.name}
+                    value={input.value || ""}
+                    onChange={input.onChange}
+                    placeholder={input.placeholder}
+                    {...(input.register ? input.register(input.name) : {})}
                 />
+
                 {input.eye}
             </div>
         </div>
     )
-    
 }
