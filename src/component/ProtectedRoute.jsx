203a9e44f3f3c1910b998e814c1b4a877605f335
alhilalfathi <alhilal.fathi@gmail.com>
2026-04-11 20:25:22 +0700

    chore: change validation variable from token to user

--- src/component/ProtectedRoute.jsx
+++ src/component/ProtectedRoute.jsx
@@ -1,9 +1,10 @@
+import { useSelector } from "react-redux"
 import { Navigate } from "react-router-dom"
 
 export const ProtectedRoute = ({ children }) => {
-    const token = localStorage.getItem("token")
+    const user = useSelector((state) => state.auth.user)
 
-    if (!token) {
+    if (!user) {
         return <Navigate to="/login" replace />
     }
 
