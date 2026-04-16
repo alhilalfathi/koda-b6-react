68e639bcfc84010d527fea0d5a7fde7754232ede
alhilalfathi <alhilal.fathi@gmail.com>
2026-04-16 09:36:57 +0700

    fix: update BASE_URL to point to the correct backend server

--- src/lib/http.js
+++ src/lib/http.js
@@ -1,4 +1,5 @@
-const BASE_URL = "https://hilal-backend.camps.fahrul.id"
+const BASE_URL = "http://68.183.226.223:20402"
+// const BASE_URL = "http://localhost:8888"
 
 async function http(url, opts={}){ 
     const headers = {
